c1f036eb229c0d6b9f8deba0dd227af605944b89
Sergey <sergey.riedel@gmail.com>
2017-08-10 17:04:26 +0400

    Init commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <h1 className="title">React Weather App</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
