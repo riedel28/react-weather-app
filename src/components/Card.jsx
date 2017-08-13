999a34d329116fa809f443398b400b5e33607783
Sergey <sergey.riedel@gmail.com>
2017-08-13 16:12:38 +0400

    Add styles to Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,9 +4,9 @@ class Card extends Component {
   render() {
     return (
       <div className="box is-2">
-        <h2>City</h2>
-        <p>20C</p>
-        <p>Sunny</p>
+        <h2 className="city-name">City</h2>
+        <p className="city-temp">20C</p>
+        <p className="city-weather">Sunny</p>
       </div>
     );
   }
