1a45aa6a9c59d64f4f644c56bbe8a90edfbaff6b
Sergey <sergey.riedel@gmail.com>
2019-06-02 19:10:20 +0200

    Add deployment options

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const apiKey = '&APPID=e027ff558bf5bf1e94c43dc79604934e';
 
 const getWeather = (city) => {
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric${apiKey}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric${apiKey}`;
 
   return fetch(url).then(response => response.json());
 };
